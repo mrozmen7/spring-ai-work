26114352003fd1cc1bd9a43d341713ad2e23a088
Yavuz Ozmen <yvz.o@Yavuz-MacBook-Pro.local>
2025-08-06 20:21:02 +0200

    Initial commit

---
+++ src/main/java/com/ozmenyavuz/SpringAiStudioApplication.java
@@ -0,0 +1,23 @@
+package com.ozmenyavuz;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("com.ozmenyavuz")
+@EntityScan(basePackages = "com.ozmenyavuz")
+@SpringBootApplication
+public class SpringAiStudioApplication {
+
+	public static void main(String[] args) {
+		// ✅ .env dosyasını yükle
+		Dotenv dotenv = Dotenv.load();
+
+		// ✅ JVM ortam değişkeni olarak ata
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+		SpringApplication.run(SpringAiStudioApplication.class, args);
+	}
+}
